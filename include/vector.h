6960b698f46e18e74e06023d03f87369e0a40e08
RokasTverijonas <tverijonasr@gmail.com>
2026-05-12 00:16:21 +0300

    Add destructor, copy assignment operator, size and capacity methods

--- include/vector.h
+++ include/vector.h
@@ -3,22 +3,12 @@ template<typename T>
 class Vector
 {
 private:
-        T* data_ = nullptr;
-        size_t size_ = 0;
-        size_t capacity_ = 0;
-
-public:
-    Vector()
-    {
-        // allocate 2 elements
-        ReAlloc(2);
-    }
+    T* data_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
 
     void ReAlloc(size_t newCapacity)
     {
-        //allocate a new block of memory
-        //Copy/move ols elemenets ono new block
-        //delete
 
         T* newBlock = new T[newCapacity];
         
@@ -37,30 +27,67 @@ public:
 
     }
 
-    void PushBack(const T& value)
+public:
+    //numatytasis konstruktorius
+    Vector() = default;
+    //destruktorius
+    ~Vector() 
+    {
+        delete[] data_;
+    }
+
+    //OPERATORS
+    //copy assignment operator
+    Vector& operator=(const Vector& other)
+    {
+        if(this == &other)
+        {
+            return *this;
+        }
+
+        T* newBlock = new T[other.capacity_];
+
+        for(size_t i = 0; i < other.size_; i++)
+        {
+            newBlock[i] = other.data_[i];
+        }
+
+        delete[] data_;
+        data_ = newBlock;
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+
+        return *this;
+    }
+
+    //negalima keisti elemento
+    const T& operator[](size_t index) const
+    {
+        return data_[index];
+    }
+    //galima keisti elementa
+    T& operator[](size_t index)
+    {
+        return data_[index];
+    }
+    
+
+    void push_back(const T& value)
     {
         
         if(size_ >= capacity_)
         {
             //jei capacity 0, priskiriam 2, jei ne tada padvigubinam capacity
-            ReAlloc(capacity == 0 ? 2 : capacity_ * 2);
+            ReAlloc(capacity_ == 0 ? 2 : capacity_ * 2);
         }
         //i tuscia vieta priskiriam elementa
         data_[size_] = value;
         size_++;
     }
 
-    size_t Size() const { return size_; }
-
-    const T& operator[](size_t index) const
-    {
-        return data_[index];
-    }
-
-    T& operator[](size_t index)
-    {
-        return data_[index];
-    }
-
+    //grazina elemnentu kieki
+    size_t size() const { return size_; }
+    //grazina talpa
+    size_t capacity() const { return capacity_; }
 
 };
